8aa125f54b362b247bb9cc1b7bd74ccd66b2158d
Marco Ciavarella <marco.ciavarella@exmergo.com>
2026-07-03 16:54:08 +0200

    `dex-core` Remove dormant OSI exporter and related schema files

--- packages/dex-core/src/exmergo_dex_core/exporters/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Exporters that project the dbt semantic model into other formats.
-
-These are layered on top of the dbt source of truth. None is emitted in v1; OSI
-is the first, kept dormant until the format matures."""
